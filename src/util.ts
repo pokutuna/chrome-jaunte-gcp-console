41b0882526c42a19c05e79e139ac6b038e418b61
pokutuna <popopopopokutuna@gmail.com>
2020-03-19 05:48:57 +0900

    compute set

--- src/util.ts
+++ src/util.ts
@@ -6,3 +6,15 @@ export function escapeXML(input: string): string {
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&apos;');
 }
+
+export function union<T>(a: Set<T>, b: Set<T>): Set<T> {
+  const set = new Set<T>(a);
+  b.forEach(i => set.add(i));
+  return set;
+}
+
+export function intersection<T>(a: Set<T>, b: Set<T>): Set<T> {
+  const set = new Set<T>();
+  a.forEach(i => b.has(i) && set.add(i));
+  return set;
+}
